80f867e3e2f633724f403a6e7ed592ba75294741
OsmarVitor <perez_vitor@hotmail.com>
2020-09-29 09:56:06 -0300

    FEAT: Add query updateRequestStage

--- src/main/java/com/user/managementuserapi/repository/RequestRepository.java
+++ src/main/java/com/user/managementuserapi/repository/RequestRepository.java
@@ -1,7 +1,9 @@
 package com.user.managementuserapi.repository;
 
 import com.user.managementuserapi.domain.Request;
+import com.user.managementuserapi.domain.enums.RequestState;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +13,8 @@ import java.util.UUID;
 public interface RequestRepository extends JpaRepository<Request, UUID> {
 
     public List<Request> findAllByUserUuid(UUID uuid);
+
+    @Query("UPDATE Request SET state = ?1 WHERE uuid = ?2")
+    public Request updateRequestStage(RequestState requestState, UUID uuid);
 
 }
